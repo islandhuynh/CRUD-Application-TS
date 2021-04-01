9ae0065e2a6a65832c085c49b7f902f5edb98d55
Island <islandhuynh@gmail.com>
2021-04-01 05:24:16 -0400

    cleaned up console logs

--- src/Shared/MethodDecorators.ts
+++ src/Shared/MethodDecorators.ts
@@ -2,9 +2,7 @@ export function logInvocation(target: Object, propertyKey: string, descriptor: P
   const className = target.constructor.name;
   let originalMethod = descriptor.value;
   descriptor.value = async function (...args: any[]) {
-    console.log(`${className}#${propertyKey} called with: ${JSON.stringify(args)}`);
     const result = await originalMethod.apply(this, args);
-    console.log(`${className}#${propertyKey} returned: ${JSON.stringify(result)}`);
     return result;
   }
 
--- src/Shared/ObjectsCounter.ts
+++ src/Shared/ObjectsCounter.ts
@@ -1,5 +1,3 @@
-
-
 export class Monitor {
 
   public static printInstances(): string {
